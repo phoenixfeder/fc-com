f816d4bf136cd132408d3485b5e9f9bf5c081c2d
Yanamoe <moritz.horch@web.de>
2018-10-20 13:00:27 +0200

    Base frame for redux created

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello World!</p>
+            </div>
+        );
+    }
+
+}
+
+export default App;
---
+++ frontend/src/containers/app-container.js
@@ -0,0 +1,6 @@
+import {connect} from 'react-redux';
+import App from '../components/App';
+
+const AppContainer = connect()(App);
+
+export default AppContainer;
