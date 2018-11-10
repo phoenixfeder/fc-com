d917570b053117d5f8eaf23dc93d03b4b817aee6
Yanamoe <moritz.horch@web.de>
2018-11-10 14:37:32 +0100

    Removed FAQ Container

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import HomeContainer from '../containers/home-container';
-import FAQContainer from '../containers/faq-container';
+import FAQ from './FAQ';
 import RegisterContainer from "../containers/register-container";
 import HelloWorldContainer from "../containers/helloworld-container";
 import EditProfileContainer from "../containers/editprofile-container";
@@ -14,7 +14,7 @@ class Main extends Component {
                 <Switch>
                     <Route exact path='/' component={HomeContainer}/>
                     <Route path='/register' component={RegisterContainer}/>
-                    <Route path='/faq' component={FAQContainer}/>
+                    <Route path='/faq' component={FAQ}/>
                     <Route path='/helloworld' component={HelloWorldContainer}/>
                     <Route path='/edit' component={EditProfileContainer}/>
                     <Route path='/login' component={LoginContainer}/>
--- frontend/src/containers/faq-container.js
+++
@@ -1,6 +0,0 @@
-import {connect} from 'react-redux';
-import FAQ from '../components/FAQ';
-
-const FAQContainer = connect()(FAQ);
-
-export default FAQContainer;
