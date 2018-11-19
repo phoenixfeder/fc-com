e9dd0caf5cee30bb10fa1ffecf6477a30d9e9099
Yanamoe <moritz.horch@web.de>
2018-11-19 22:01:29 +0100

    Reverted width #FCC-48

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -215,7 +215,7 @@ class Register extends Component {
             <div className={classes.root}>
                 <MuiThemeProviderUI theme={lightTheme}>
                     <Grid container alignContent="center" justify="center">
-                        <Grid item xs={12} md={12} lg={12}>
+                        <Grid item xs={12} md={8} lg={4}>
                             <Paper className={classes.root} elevation={1}>
                                 <Grid container spacing={16} alignItems="center" justify="space-evenly"
                                       direction="column">
