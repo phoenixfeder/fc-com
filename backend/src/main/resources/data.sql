59eab84816057456f7af23937617f8ad5107b244
Sascha Grotz <sascha.grotz@web.de>
2018-11-25 13:33:21 +0100

    test entries for Frontend testing

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -7,6 +7,10 @@ INSERT INTO user(id, username, email, password, role_id) VALUES
 (1, 'testuser', 'test.user@test.de', '123456', 1),
 (2, 'expireduser', 'expired.user@fc.de', '123456', 1);
 
+INSERT INTO verification_token(id, expiry_date, token, user_id) VALUES
+(1, '2030-03-03 00:00:00', 'abcdefghij', 1),
+(2, '2010-03-03 00:00:00', 'abcdefghij', 2);
+
 INSERT INTO role(id, name) VALUES
 (1, 'user'),
 (2, 'admin');
