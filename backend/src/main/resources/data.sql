e3e5029bfbc11447ba945849c94cbb420167d623
Sascha Grotz <sascha.grotz@web.de>
2018-12-02 13:36:28 +0100

    fix compile error

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -5,8 +5,8 @@ INSERT INTO flash_card(id, back_text, front_text, title) VALUES
 
 INSERT INTO user(id, username, email, password, role_id, enabled) VALUES
 (1, 'testuser', 'test.user@fc.de', '$2a$10$gba/UpsDbHZmPCMUS4LkEeO8fY53BmGfmNgrObtWQ8zcW1BYWC/Im', 1, false),
-(2, 'expireduser', 'expired.user@fc.de', '$2a$10$cibeyz8xZSR5Ohc.hA7n6ePfsWagaOk.VHa8VGICBiwvPywRSYttm', 1, false);
-(3, 'enableduser', 'enabled.user@fc.de', '$2a$10$cibeyz8xZSR5Ohc.hA7n6ePfsWagaOk.VHa8VGICBiwvPywRSYttm', 1, true)
+(2, 'expireduser', 'expired.user@fc.de', '$2a$10$cibeyz8xZSR5Ohc.hA7n6ePfsWagaOk.VHa8VGICBiwvPywRSYttm', 1, false),
+(3, 'enableduser', 'enabled.user@fc.de', '$2a$10$cibeyz8xZSR5Ohc.hA7n6ePfsWagaOk.VHa8VGICBiwvPywRSYttm', 1, true);
 
 INSERT INTO verification_token(id, expiry_date, token, user_id) VALUES
 (1, '2030-03-03 00:00:00', 'abcdefghij', 1),
