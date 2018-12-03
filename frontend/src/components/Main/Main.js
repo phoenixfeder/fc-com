b4d3e6bb6824c802e2b043cc44a62e46829e083e
Yanamoe <moritz.horch@web.de>
2018-12-03 17:39:07 +0100

    Fixed routing issue #FCC-92
    /edit?foo=bar redirected to home. Now fixed.

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import HelloWorldContainer from "../HelloWorld/helloworld-container";
 import EditProfile from "../EditProfile/EditProfile";
@@ -26,7 +26,7 @@ class Main extends Component {
                 <Route path='/faq' component={FAQ} />
                 <Route path='/helloworld' component={HelloWorldContainer} />
                 <Route path='/verify' component={VerfiyContainer} />
-                <Redirect to="/" />
+                <Route component={Home} />
             </Switch>
         );
 
@@ -38,7 +38,7 @@ class Main extends Component {
                     <Route path='/edit' component={EditProfile} />
                     <Route path='/faq' component={FAQ}/>
                     <Route path='/helloworld' component={HelloWorldContainer}/>
-                    <Redirect to="/" />
+                    <Route component={Home} />
                 </Switch>
             );
         }
