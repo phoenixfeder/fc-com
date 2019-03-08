f48eab4da1c5792dfcff7ea6914f5d3e8dcc8b8d
Yanamoe <moritz.horch@web.de>
2019-03-08 23:48:30 +0100

    Formatted all actions to new style guide #FCC-120
    Flashcard and Snackbar Actions are now formatted.

--- frontend/src/actions/notistack-snackbar-actions.js
+++ frontend/src/actions/notistack-snackbar-actions.js
@@ -1,12 +1,12 @@
 export const enqueueSnackbar = notification => ({
-    type: "ENQUEUE_SNACKBAR",
-    notification: {
-        key: new Date().getTime() + Math.random(),
-        ...notification
-    }
+  type: 'ENQUEUE_SNACKBAR',
+  notification: {
+    key: new Date().getTime() + Math.random(),
+    ...notification,
+  },
 });
 
 export const removeSnackbar = key => ({
-    type: "REMOVE_SNACKBAR",
-    key
+  type: 'REMOVE_SNACKBAR',
+  key,
 });
