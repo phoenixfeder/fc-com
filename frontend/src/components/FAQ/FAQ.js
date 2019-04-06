fbc1b19e351a29bc943c20078758556abcecc1ba
Nifori <benjamin.duesterhues@gmail.com>
2019-04-06 17:31:58 +0200

    FCC-128 Created Files

--- frontend/src/components/FAQ/FAQ.js
+++ frontend/src/components/FAQ/FAQ.js
@@ -1,9 +1,6 @@
-import {
-  Grid,
-  Paper,
-  Typography,
-  withStyles,
-} from '@material-ui/core/';
+import { ListItem } from '@material-ui/core';
+import { Grid, Paper, Typography, withStyles } from '@material-ui/core/';
+import List from '@material-ui/core/List';
 import * as PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
@@ -45,10 +42,30 @@ class FAQ extends Component {
             >
               <Grid item lg={12}>
                 <Typography variant="h3" align="center">Friendly Asked Questions</Typography>
-                <Typography variant="body1" align="center">Here you will be able to find answers to a lot of possible questions!</Typography>
+                <Typography variant="body1" align="center">Here you will be able to find answers to a lot of possible
+                  questions!</Typography>
               </Grid>
               <Grid item lg={12}>
-                <Typography variant="body1" align="center">Im currently just a placeholder. Soon there will be more to see!</Typography>
+                <List>
+                  <ListItem>
+                    <Typography variant="body1"><b>Will this application be available for free?</b>
+                    </Typography>
+                  </ListItem>
+                  <ListItem>
+                    <Typography variant="body1">Yes! We intend to help you learn as good and fast as you
+                      can. So restricting your access is no possible option.</Typography>
+                  </ListItem>
+
+                  <ListItem>
+                    <Typography variant="body1"><b>Will my grade improve by using the application?</b>
+                    </Typography>
+                  </ListItem>
+                  <ListItem>
+                    <Typography variant="body1">We try to help you improve your grade. However we can not
+                      guarantee any improvements. It all depends on your commitment.</Typography>
+                  </ListItem>
+
+                </List>
               </Grid>
             </Grid>
           </Grid>
