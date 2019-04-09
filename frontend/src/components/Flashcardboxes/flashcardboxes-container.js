fe758e84df846ff5f0a81734711bdf3677d7dc3f
Yanamoe <moritz.horch@web.de>
2019-04-09 11:45:39 +0200

    Added Flashcardboxes site and integrated into menu #FCC-138

---
+++ frontend/src/components/Flashcardboxes/flashcardboxes-container.js
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+import Flashcardboxes from './Flashcardboxes';
+
+const HelloWorldContainer = connect(null, null)(Flashcardboxes);
+
+export default HelloWorldContainer;
