7d46f80122811bce845b0916bfcbbb39bfb39d7c
Yanamoe <moritz.horch@web.de>
2019-04-10 18:59:33 +0200

    Moved Flashcardbox to an own component #FCC-138

--- frontend/src/components/Flashcardboxes/Flashcardboxes.js
+++ frontend/src/components/Flashcardboxes/Flashcardboxes.js
@@ -1,24 +1,11 @@
 import {
   Grid,
-  Paper,
-  Chip,
   Typography,
   withStyles,
-  Card,
-  CardActions,
-  CardContent,
-  Button,
-  IconButton,
 } from '@material-ui/core/';
-import {
-  FileCopy,
-  SentimentSatisfiedAlt,
-  SentimentDissatisfied,
-  Edit,
-  Delete,
-} from '@material-ui/icons';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Flashcardbox from './Flashcardbox';
 
 const styles = theme => ({
   root: {
@@ -55,48 +42,19 @@ class Flashcardboxes extends Component {
   }
 
   renderCards = () => {
-    const { classes } = this.props;
     const cards = [];
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < 30; i++) {
       const successRate = Math.floor(Math.random() * Math.floor(100));
       const amountCards = Math.floor(Math.random() * Math.floor(1000));
-      let successChip = <Chip label={`${successRate}% correct`} className={classes.infoChipSuccess} icon={<SentimentSatisfiedAlt />} color="primary" />;
-      if (successRate <= 50) {
-        successChip = <Chip label={`${successRate}% correct`} className={classes.infoChip} icon={<SentimentDissatisfied />} color="secondary" />;
-      }
       cards.push((
         <Grid item xs={6} md={4} lg={3} key={i}>
-          <Card>
-            <CardContent>
-              <Typography
-                className={classes.title}
-                color="textSecondary"
-                gutterBottom
-              >
-                {'Flashcardbox'}
-              </Typography>
-              <Typography variant="h5" component="h2">
-                {'Title'}
-              </Typography>
-              <Typography component="p">
-                {'Im a description of a very cool flashcardbox and my author made me extra long for testing purposes only.'}
-              </Typography>
-              { successChip }
-              <Chip label={`${amountCards} cards`} className={classes.infoChip} icon={<FileCopy />} color="primary" />
-            </CardContent>
-            <CardActions disableActionSpacing>
-              <Button size="medium">Learn</Button>
-              <div style={{ width: '100%', textAlign: 'right' }}>
-                <IconButton aria-label="Share">
-                  <Edit />
-                </IconButton>
-                <IconButton aria-label="Edit Flashcardbox">
-                  <Delete />
-                </IconButton>
-              </div>
-            </CardActions>
-          </Card>
+          <Flashcardbox
+            title="Title"
+            amount={amountCards}
+            successRate={successRate}
+            description="Im a description of a very cool flashcardbox and my author made me extra long for testing purposes only."
+          />
         </Grid>
       ));
     }
@@ -107,14 +65,14 @@ class Flashcardboxes extends Component {
     const { classes } = this.props;
     return (
       <div className={classes.root} id="Flashcardboxes">
-        <Grid container justify="center">
+        <Grid container >
           <Grid item lg={12} className={classes.headline}>
             <Typography variant="h3" align="center">
               {'My Flashcardboxes'}
             </Typography>
           </Grid>
           <Grid container direction="row" justify="space-evenly" spacing={16}>
-            {this.renderCards()}
+            { this.renderCards() }
           </Grid>
         </Grid>
       </div>
