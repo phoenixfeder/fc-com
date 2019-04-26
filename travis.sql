12757266ec3fb46dafaa1ee60cabbfb1ba50b84f
Yanamoe <moritz.horch@web.de>
2019-04-26 15:08:55 +0200

    Adding a database for running the backend #FCC-157

---
+++ travis.sql
@@ -0,0 +1,6 @@
+# Create DB
+CREATE DATABASE IF NOT EXISTS `fcc_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+# Create Testuser
+CREATE USER 'fcc'@'localhost' IDENTIFIED BY 'fccpassword';
+GRANT ALL ON 'fcc_db' TO 'fcc'@'localhost';
