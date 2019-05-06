92e0f51febebe76a6f02befe0560c4a603e715f8
Yanamoe <moritz.horch@web.de>
2019-05-06 16:24:19 +0200

    Removed Hello World from whole page as endpoint does not exist anymore

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -9,7 +9,6 @@ import EditProfileContainer from '../EditProfile/EditProfile-container';
 import FAQ from '../FAQ/FAQ';
 import FlashcardOverviewContainer from '../Flashcard_Overview/FlashcardOverview-container';
 import FlashcardboxesContainer from '../Flashcardboxes/flashcardboxes-container';
-import HelloWorldContainer from '../HelloWorld/helloworld-container';
 import Home from '../Home/Home';
 import LoginContainer from '../Login/login-container';
 import LogoutContainer from '../Logout/logout-container';
@@ -29,7 +28,6 @@ class Main extends Component {
         <Route path="/login" component={LoginContainer} />
         <Route path="/register" component={RegisterContainer} />
         <Route path="/faq" component={FAQ} />
-        <Route path="/helloworld" component={HelloWorldContainer} />
         <Route path="/verify" component={VerfiyContainer} />
         <Route component={Home} />
       </Switch>
@@ -42,7 +40,6 @@ class Main extends Component {
           <Route path="/logout" component={LogoutContainer} />
           <Route path="/edit" component={EditProfileContainer} />
           <Route path="/faq" component={FAQ} />
-          <Route path="/helloworld" component={HelloWorldContainer} />
           <Route path="/boxes" component={FlashcardboxesContainer} />
           <Route path="/flashcards" component={FlashcardOverviewContainer} />
           <Route component={Home} />
