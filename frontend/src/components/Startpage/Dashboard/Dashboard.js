30be1e5e997f3732af6ea161d8436085d60668ac
Yanamoe <moritz.horch@web.de>
2019-05-23 16:45:23 +0200

    Added different home page for authenticated user #FCC-189

---
+++ frontend/src/components/Startpage/Dashboard/Dashboard.js
@@ -0,0 +1,62 @@
+import {
+  Grid,
+  Typography,
+  withStyles,
+} from '@material-ui/core/';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 2,
+    flexGrow: 1,
+  },
+  headline: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  text: {
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2,
+  },
+});
+
+class Dashboard extends Component {
+  componentDidMount() {
+    document.title = 'Home';
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Grid container justify="center">
+          <Grid item sm={12} md={8} lg={6}>
+            <Grid
+              container
+              justify="center"
+              spacing={16}
+              elevation={2}
+              direction="column"
+            >
+              <Grid item lg={12}>
+                <Typography variant="h3" align="center" className={classes.text}>Welcome Username!</Typography>
+              </Grid>
+              <Grid item lg={12}>
+                <Typography variant="body1" className={classes.text}>
+                  {'Hi there! There! There!'}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Dashboard);
